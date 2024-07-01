24038d7577405dabcdf6efc1f556de6fe28ffb5d
Nandan Pathak <nandanpathak30@gmail.com>
2024-07-01 22:57:46 +0530

    bug solved

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -58,8 +58,8 @@ export default function Home() {
                 </div>
             </div>
             <img className="absolute top-[3.125rem] h-[25rem]" src={figure10} alt="Figure 10" />
-            <img className="absolute bg-r top-[29.9rem] h-[10rem] left-[1.875rem]" src={figure9} alt="Figure 9" />
-            <img className="hidden md:flex md:absolute md:top-[10.58rem] md:h-[30.37rem] md:left-[70.2rem]" src={group} alt="Group" />
+            <img className="absolute bg-r top-[29.9rem] h-[10.5rem] left-[1.875rem]" src={figure9} alt="Figure 9" />
+            <img className="hidden md:flex md:absolute md:top-[10.58rem] md:h-[28.37rem] md:left-[68.2rem]" src={group} alt="Group" />
         </div>
     );
 }
